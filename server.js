057c7c5177ce535ed33a9eb04f91bcec004e34d4
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-29 13:55:25 -0700

    Added get's for / and /notes and added middleware for statics

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ let app = express();
 //define port for local and heroku
 let PORT = process.env.PORT || 3000
 
+//middle ware for static files
+app.use(express.static('public'))
+
+//define first get for / to direct to /public/index.html
+app.get("/", (req, res) => {
+    //serving the index using path to join the paths
+    res.sendFile(path.join(__dirname, "./public", "index.html")); 
+
+});
+
+//define get for /notes to serve notes.html
+app.get("/notes", (req, res) => {
+    //serving the index using path to join the paths
+    res.sendFile(path.join(__dirname, "./public", "notes.html"))   
+});
 
 //define listen
 app.listen(PORT, () => {
