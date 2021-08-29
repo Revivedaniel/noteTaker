8722c3c68550234333821b9da8f8af9258a44a0f
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-29 14:32:47 -0700

    Required the db.json, added json middle ware, and created api/notes call

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 //require path
 const path = require("path");
+//require db.json
+const db = require("./db/db.json")
 
 //define express as app
 let app = express();
@@ -11,6 +13,9 @@ let PORT = process.env.PORT || 3000
 
 //middle ware for static files
 app.use(express.static('public'))
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //define first get for / to direct to /public/index.html
 app.get("/", (req, res) => {
@@ -24,6 +29,13 @@ app.get("/notes", (req, res) => {
     //serving the index using path to join the paths
     res.sendFile(path.join(__dirname, "./public", "notes.html"))   
 });
+
+//define get for /api/notes
+app.get("/api/notes", (req, res) => {
+    res.json(db)
+})
+//define post for /api/notes
+//define delete for /api/notes
 
 //define listen
 app.listen(PORT, () => {
